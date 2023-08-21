4032253d1ed94be1d1b2cc769c4434988a2ebf6c
Khoa15 <nguyentrongdangkhoa15@gmail.com>
2023-08-21 11:43:09 +0700

    Done lab02

---
+++ lab02/Bai3/Bai3/Models/BookModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bai3.Models
+{
+    public class BookModel
+    {
+        [Key]
+        public String Id { get;set; }
+        public string Title { get;set; }
+        public string Avatar { get; set; }
+        public ProducerModel Producer { get; set; }
+        public BookModel() {
+            Id = String.Empty;
+            Title = String.Empty;
+            Avatar = String.Empty;
+            Producer = new ProducerModel();
+        }
+
+        public BookModel(string id, string title, string avatar, ProducerModel producer)
+        {
+            Id = id;
+            Title = title;
+            Avatar = avatar;
+            Producer = producer;
+        }
+    }
+}
