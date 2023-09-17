89f9fd4696c47d68cb47d216ed180c7d53392308
Khoa15 <nguyentrongdangkhoa15@gmail.com>
2023-09-17 23:37:12 +0700

    bai3

---
+++ lab04/Bai3/Bai3/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bai3.Models
+{
+    public class UserModel
+    {
+        [Required()]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required()]
+        [StringLength(100)]
+        public string Email { get; set; }
+        [Required()]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "'ConfirmPassword' and 'Password' do not match")]
+        public string ReTypePassword { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
